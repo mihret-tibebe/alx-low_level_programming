fec7aa23d26fc21b1d8976c909f89171f2b42d43
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-10-03 21:33:58 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,11 +15,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	if (size == 0)
-		return NULL;
+		return (NULL);
 
 	ch = (char*) malloc (size * sizeof (char));
 	if (ch == NULL)
-		return NULL;
+		return (NULL);
 
 	for (i = 0; i < size; i++) 
 	{
