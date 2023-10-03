8f7a8a7183b8ce94e9c320532fb1b934abb7457a
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-10-03 22:36:10 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,24 +10,25 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	
+
 	char *ch;
 	int len1, len2, i, j, k;
+
 	len1 = 0;
 	len2 = 0;
 	j = 0;
 	k = 0;
 
-	while(s1[len1])
+	while (s1[len1])
 		len1++;
 
-	while(s2[len2])
+	while (s2[len2])
 		len2++;
 
 	ch = malloc((len1 + len2 + 1) * sizeof(char));
 
 	if (ch == NULL)
-		return (NULL); 
+		return (NULL);
 
 	for (i = 0; i < len1; i++)
 		{
