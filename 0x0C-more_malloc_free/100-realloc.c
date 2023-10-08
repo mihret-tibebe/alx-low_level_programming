8bd88ed06df89b5eabe9085bd06dd021e46b60ed
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-10-08 17:20:04 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,53 +3,54 @@
 
 /**
  * _realloc - reallocates a memory block using malloc and free
- * @ptr:  pointer to previously allocated memory
+ * @ptr: pointer to previously allocated memory
  * @old_size: size of the old allocation in bytes
  * @new_size: size of the new allocation in bytes
- * Return: new reallocated memory
+ * 
+ * Return: pointer to the newly allocated memory block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *mem;
-	char *ch;
-	char *c;
-	unsigned int i = 0;
+    void *mem;
+    char *ch;
+    char *c;
+    unsigned int i = 0;
 
-	if (new_size == old_size)
-		return (ptr);
+    if (new_size == old_size)
+        return (ptr);
 
-	if (ptr == NULL)
-	{
-		mem = malloc(new_size);
-		if (!mem)
-			return (NULL);
-		ch = (char *) mem;
-	}
+    if (ptr == NULL)
+    {
+        mem = malloc(new_size);
+        if (!mem)
+            return (NULL);
+        ch = (char *)mem;
+    }
 
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
+    if (new_size == 0 && ptr != NULL)
+    {
+        free(ptr);
+        return (NULL);
+    }
 
-	if (new_size > old_size && ptr != NULL)
-	{
-		c = (char *) ptr;
+    if (new_size > old_size && ptr != NULL)
+    {
+        c = (char *)ptr;
 
-		mem = malloc(new_size);
+        mem = malloc(new_size);
 
-		if (!mem)
-			return (NULL);
-		ch = (char *) mem;
+        if (!mem)
+            return (NULL);
+        ch = (char *)mem;
 
-		while (i < old_size)
-		{
-			ch[i] = c[i];
-			i++;
-		}
+        while (i < old_size)
+        {
+            ch[i] = c[i];
+            i++;
+        }
 
-		free(ptr);
-	}
+        free(ptr);
+    }
 
-	return (mem);
+    return (mem);
 }
