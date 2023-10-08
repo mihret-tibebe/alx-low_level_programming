0fbe6c386f0f22b7a0533650342cf6ea3ba78ef3
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-10-08 14:47:40 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,12 +38,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (ch == NULL)
 		return (NULL);
 
-	while (i <= len1)
+	for (i = 0; i <= len1; i++)
 	{
 		ch[i] = s1[i];
-		i++;
 	}
-	i += len1;
+	i = len1;
 
 	while (i <= (len1 + len))
 	{
