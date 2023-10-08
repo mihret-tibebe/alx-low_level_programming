782835cc21a18c96e514d90c0c4de316ba77a916
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-10-08 12:27:55 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,1 +1,40 @@
+#include <stdlib.h>
+#include "main.h"
 
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string
+ * @s2: string
+ * Return: a pointer that point to a newly allocated 
+ * space in memory, which contains s1, followed by the 
+ * first n bytes of s2, and null terminated
+ */
+int word_count(char *s1)
+{
+	int len = 0;
+
+	while(s1[len])
+	{
+		len++;
+	}
+
+	return (len);
+}
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ch;
+	int len1, len2, len;
+
+	len1 = word_count(s1);
+	len2 = word_count(s2);
+
+	if (n >= len2)
+		len = len2;
+	len = n;
+
+	ch = malloc(sizeof(char) * ( len1 + len ));
+	if (ch == NULL)
+		return (NULL);
+
+	return (ch);
+}
