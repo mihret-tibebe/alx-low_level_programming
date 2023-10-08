02c9fedfdf5f5870b3d207de94ca79acd4dca9ce
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-10-08 16:31:02 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,7 +18,7 @@ unsigned int calc_min(unsigned int o, unsigned int n)
  * _realloc - reallocates a memory block using malloc and free
  * @ptr:  pointer to previously allocated memory
  * @old_size: old size of ptr
- * new_size: new size of ptr
+ * @new_size: new size of ptr
  * 
  * Return: string length
 */
