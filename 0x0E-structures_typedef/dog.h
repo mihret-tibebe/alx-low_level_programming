6c94f2ecc7a82799e572c397b4ab144f64d0ccf3
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-10-11 00:56:17 +0300

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,11 @@
+/**
+ * struct dog - struct for dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+*/
+typedef struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
