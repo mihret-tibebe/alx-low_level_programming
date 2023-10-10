37f701cf881c004cc104e9974fb8e4c18a739587
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-10-11 01:40:43 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,7 +3,7 @@
 
 void print_dog(struct dog *d)
 {
-	printf("Name: \s%\n", d->name);
-	printf("Age: \d%\n", d->age);
-	printf("Owner: \s%\n", d->owner);
+	printf("Name: %s\n", d->name);
+	printf("Age: %d\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
