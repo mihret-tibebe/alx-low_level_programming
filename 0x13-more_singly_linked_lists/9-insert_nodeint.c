9cf7603a423bcc3b8dd420972586025e6775833a
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2023-10-24 01:04:40 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,76 +1,51 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a node at the specified index @idx if it
- * is possible, otherwise it returns NULL
- * @head: the first node of the linked list
- * @idx: the index where the new node is to be added
- * @n: the value of the new node
+ * insert_nodeint_at_index - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is
+ * added.
+ * @n: integer element.
  *
- * Return: newly added node or NULL if function fails
+ * Return: the address of the new node, or NULL if it
+ * failed.
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i, len;
-	listint_t *first_node, *new_node, *next_to_new_node;
+	unsigned int i;
+	listint_t *new;
+	listint_t *h;
 
-	if (!head)
-		return (NULL);
+	h = *head;
 
-	len = list_len(*head);
-	first_node = *head;
-	new_node = malloc(sizeof(listint_t));
-
-	if (!new_node)
+	if (idx != 0)
 	{
-		free(new_node);
-		return (NULL);
+		for (i = 0; i < idx - 1 && h != NULL; i++)
+		{
+			h = h->next;
+		}
 	}
 
-	new_node->n = n;
-	new_node->next = NULL;
-	if (!len)
+	if (h == NULL && idx != 0)
 		return (NULL);
-	else if (idx + 1 > len + 1)
-	{
-		return (NULL);
-	}
-	else if (idx != 0)
-	{
-		for (i = 0; i < idx - 1; i++)
-			*head = (*head)->next;
-		next_to_new_node = (*head)->next;
 
-		(*head)->next = new_node;
-		new_node->next = next_to_new_node;
-		*head = first_node;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
 	}
 	else
 	{
-		new_node->next = *head;
-		*head = new_node;
-	}
-	return (new_node);
-}
-
-/**
- * list_len - get the number of nodes in the linked list @h
- * @h: the linked list whose size is to be calculated
- *
- * Return: the size of the list
- */
-unsigned int list_len(listint_t *h)
-{
-	unsigned int nodes = 0;
-	listint_t *first_node = h;
-
-	while (h != NULL)
-	{
-		nodes++;
-		h = h->next;
+		new->next = h->next;
+		h->next = new;
 	}
 
-	h = first_node;
-	return (nodes);
+	return (new);
 }
