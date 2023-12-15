86350fab6eaa6a98f305a5960b5aa6011c5055f9
Mihret Tefera <99690582+MihretTibebe@users.noreply.github.com>
2023-12-15 06:39:28 +0300

    functions

---
+++ 0x18-dynamic_libraries/functions/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char myStr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(myStr);
+
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/functions/_strpbrk.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strpbrk- strpbrk function
+ * @s: 1st argument
+ * @accept: 2nd argument
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (0);
+}
